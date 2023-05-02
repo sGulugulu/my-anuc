4f9b90288c9b5e00f08c1bd4950d2d5ad7066d4c
annuasd <807239819@qq.com>
2023-05-02 14:23:08 +0800

    改了instruction的getResult方法“

---
+++ lib/Analysis/livenessAnalysis.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 牛奕博 on 2023/5/2.
+//
+
+#include "livenessAnalysis.h"
---
+++ lib/Analysis/livenessAnalysis.h
@@ -0,0 +1,16 @@
+//
+// Created by 牛奕博 on 2023/5/2.
+//
+
+#ifndef ANUC_LIVENESSANALYSIS_H
+#define ANUC_LIVENESSANALYSIS_H
+
+
+
+class livenessAnalysis {
+
+};
+
+
+
+#endif //ANUC_LIVENESSANALYSIS_H
